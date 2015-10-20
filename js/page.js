7f791f8c1390265e2a673c4c63a505b140fdc531
Shengzhang Wen <coffeesherk@gmail.com>
2015-10-20 22:57:38 +0800

    add pager

---
+++ js/page.js
@@ -0,0 +1,33 @@
+/**
+ * Created by geminiwen on 15/10/20.
+ */
+$(function () {
+
+    var nextPage = function () {
+        var index = $('.current').index();
+        var $next = $(".screen").get(index);
+        if ($next == undefined) {
+            return;
+        }
+        $($next).show()
+            .css({
+                "z-index": -1
+            })
+        $('.current').css({
+            "transition": "transform 1s linear",
+            "transform": "translateY(-100%)",
+            "z-index": 1
+        }).on("transitionend", function () {
+            $(this).removeClass("current").hide();
+            $($next).addClass("current")
+                .css({
+                    "z-index": 1
+                })
+        });
+
+    };
+
+    $("#detail-btn").click(function () {
+        nextPage();
+    })
+});
